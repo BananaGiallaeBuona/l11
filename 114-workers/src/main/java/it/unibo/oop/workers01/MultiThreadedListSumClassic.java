4221bf7584012bdbdca024a5f07080910ab8c25c
DanySK <danilo.pianini@gmail.com>
2025-02-17 16:36:15 +0000

    Deploying to exercises from @ APICe-at-DISI/exercises@58edae9443d32d0e99629042d908ea3f130d2c01 🚀

--- 114-workers/src/main/java/it/unibo/oop/workers01/MultiThreadedListSumClassic.java
+++ 114-workers/src/main/java/it/unibo/oop/workers01/MultiThreadedListSumClassic.java
@@ -5,62 +5,19 @@ import java.util.List;
 
 /**
  * This is a standard implementation of the calculation.
- * 
+ *
  */
 public final class MultiThreadedListSumClassic implements SumList {
 
     private final int nthread;
 
     /**
-     * 
+     *
      * @param nthread
      *            no. of thread performing the sum.
      */
     public MultiThreadedListSumClassic(final int nthread) {
         this.nthread = nthread;
-    }
-
-    private static class Worker extends Thread {
-        private final List<Integer> list;
-        private final int startpos;
-        private final int nelem;
-        private long res;
-
-        /**
-         * Build a new worker.
-         * 
-         * @param list
-         *            the list to sum
-         * @param startpos
-         *            the initial position for this worker
-         * @param nelem
-         *            the no. of elems to sum up for this worker
-         */
-        Worker(final List<Integer> list, final int startpos, final int nelem) {
-            super();
-            this.list = list;
-            this.startpos = startpos;
-            this.nelem = nelem;
-        }
-
-        @Override
-        @SuppressWarnings("PMD.SystemPrintln")
-        public void run() {
-            System.out.println("Working from position " + startpos + " to position " + (startpos + nelem - 1));
-            for (int i = startpos; i < list.size() && i < startpos + nelem; i++) {
-                this.res += this.list.get(i);
-            }
-        }
-
-        /**
-         * Returns the result of summing up the integers within the list.
-         * 
-         * @return the sum of every element in the array
-         */
-        public long getResult() {
-            return this.res;
-        }
-
     }
 
     @Override
@@ -97,5 +54,48 @@ public final class MultiThreadedListSumClassic implements SumList {
          * Return the sum
          */
         return sum;
+    }
+
+    private static class Worker extends Thread {
+        private final List<Integer> list;
+        private final int startpos;
+        private final int nelem;
+        private long res;
+
+        /**
+         * Build a new worker.
+         *
+         * @param list
+         *            the list to sum
+         * @param startpos
+         *            the initial position for this worker
+         * @param nelem
+         *            the no. of elems to sum up for this worker
+         */
+        Worker(final List<Integer> list, final int startpos, final int nelem) {
+            super();
+            this.list = list;
+            this.startpos = startpos;
+            this.nelem = nelem;
+        }
+
+        @Override
+        @SuppressWarnings("PMD.SystemPrintln")
+        public void run() {
+            System.out.println("Working from position " + startpos + " to position " + (startpos + nelem - 1));
+            for (int i = startpos; i < list.size() && i < startpos + nelem; i++) {
+                this.res += this.list.get(i);
+            }
+        }
+
+        /**
+         * Returns the result of summing up the integers within the list.
+         *
+         * @return the sum of every element in the array
+         */
+        public long getResult() {
+            return this.res;
+        }
+
     }
 }
