31cfbf8e49136195e3c70a7eb88364adde57bd89
DanySK <danilo.pianini@gmail.com>
2025-02-17 16:44:22 +0000

    Deploying to exercises from @ APICe-at-DISI/exercises@0df4563d407a65702b739a3db92b578d8c4ce817 🚀

--- 114-workers/src/main/java/it/unibo/oop/workers01/MultiThreadedListSumWithStreams.java
+++ 114-workers/src/main/java/it/unibo/oop/workers01/MultiThreadedListSumWithStreams.java
@@ -5,7 +5,7 @@ import java.util.stream.IntStream;
 
 /**
  * This is an implementation using streams.
- * 
+ *
  */
 @SuppressWarnings("CPD-START")
 public final class MultiThreadedListSumWithStreams implements SumList {
@@ -13,55 +13,12 @@ public final class MultiThreadedListSumWithStreams implements SumList {
     private final int nthread;
 
     /**
-     * 
+     *
      * @param nthread
      *            no. of thread performing the sum.
      */
     public MultiThreadedListSumWithStreams(final int nthread) {
         this.nthread = nthread;
-    }
-
-    private static class Worker extends Thread {
-        private final List<Integer> list;
-        private final int startpos;
-        private final int nelem;
-        private long res;
-
-        /**
-         * Build a new worker.
-         * 
-         * @param list
-         *            the list to sum
-         * @param startpos
-         *            the initial position for this worker
-         * @param nelem
-         *            the no. of elems to sum up for this worker
-         */
-        Worker(final List<Integer> list, final int startpos, final int nelem) {
-            super();
-            this.list = list;
-            this.startpos = startpos;
-            this.nelem = nelem;
-        }
-
-        @Override
-        @SuppressWarnings("PMD.SystemPrintln")
-        public void run() {
-            System.out.println("Working from position " + startpos + " to position " + (startpos + nelem - 1));
-            for (int i = startpos; i < list.size() && i < startpos + nelem; i++) {
-                this.res += this.list.get(i);
-            }
-        }
-
-        /**
-         * Returns the result of summing up the integers within the list.
-         * 
-         * @return the sum of every element in the array
-         */
-        public long getResult() {
-            return this.res;
-        }
-
     }
 
     @Override
@@ -90,9 +47,52 @@ public final class MultiThreadedListSumWithStreams implements SumList {
             try {
                 target.join();
                 joined = true;
-            } catch (InterruptedException e) {
+            } catch (final InterruptedException e) {
                 e.printStackTrace();
             }
         }
+    }
+
+    private static class Worker extends Thread {
+        private final List<Integer> list;
+        private final int startpos;
+        private final int nelem;
+        private long res;
+
+        /**
+         * Build a new worker.
+         *
+         * @param list
+         *            the list to sum
+         * @param startpos
+         *            the initial position for this worker
+         * @param nelem
+         *            the no. of elems to sum up for this worker
+         */
+        Worker(final List<Integer> list, final int startpos, final int nelem) {
+            super();
+            this.list = list;
+            this.startpos = startpos;
+            this.nelem = nelem;
+        }
+
+        @Override
+        @SuppressWarnings("PMD.SystemPrintln")
+        public synchronized void run() {
+            System.out.println("Working from position " + startpos + " to position " + (startpos + nelem - 1));
+            for (int i = startpos; i < list.size() && i < startpos + nelem; i++) {
+                this.res += this.list.get(i);
+            }
+        }
+
+        /**
+         * Returns the result of summing up the integers within the list.
+         *
+         * @return the sum of every element in the array
+         */
+        public synchronized long getResult() {
+            return this.res;
+        }
+
     }
 }
