c8b0a19e5108b27065d46e85be022cca4f5b1af4
Nikita Piraino <nikita.piraino@studio.unibo.it>
2025-12-03 16:31:23 +0100

    now my code is written to do everything, it passes the tests, but need to be adapted to pass the build tests

---
+++ 113-workers/src/main/java/it/unibo/oop/workers02/SumMatrixImplementation.java
@@ -0,0 +1,111 @@
+package it.unibo.oop.workers02;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * our goal is to :
+ * 1)obtain the number of cells in the matrix
+ * 2)divide it for somthing 
+ */
+
+public class SumMatrixImplementation implements SumMatrix {
+    private final int nthread;
+    
+    SumMatrixImplementation (int nthread) {
+        this.nthread = nthread;
+    }
+
+    @Override
+    public double sum(double[][] matrix) {
+        return sum(transformInList(matrix));
+    }
+
+    public double sum(List<Double> matrixList){
+        final int size = matrixList.size() % nthread + matrixList.size()/this.nthread;
+        double sum = 0;
+        /*
+         * Build a list of workers
+         */
+        final List<Worker> workers = new ArrayList<>(nthread);
+        for (int start = 0; start < matrixList.size(); start += size) {
+            workers.add(new Worker(matrixList, start, size));
+        }
+        /*
+         * Start them
+         */
+        for (final Worker w: workers) {
+            w.start();
+        }
+        /*
+         * Wait for every one of them to finish. This operation is _way_ better done by
+         * using barriers and latches, and the whole operation would be better done with
+         * futures.
+         */
+        for (final Worker w: workers) {
+            try {
+                w.join();
+                sum += w.getResult();
+            } catch (final InterruptedException e) {
+                throw new IllegalStateException(e);
+            }
+        }
+        /*
+         * Return the sum
+         */
+        return sum;
+    }
+
+    private List<Double> transformInList(double[][] matrix){
+        List<Double> list = new ArrayList<>();
+        for (double[] row : matrix) {
+            for (double val : row) {
+                list.add(val);
+            }
+        }
+        return list;
+    }
+
+    private static class Worker extends Thread {
+        private final List<Double> list;
+        private final int startpos;
+        private final int nelem;
+        private double res;
+
+        /**
+         * Build a new worker.
+         *
+         * @param list
+         *            the list to sum
+         * @param startpos
+         *            the initial position for this worker
+         * @param nelem
+         *            the no. of elems to sum up for this worker
+         */
+        Worker(final List<Double> list, final int startpos, final int nelem) {
+            super();
+            this.list = list;
+            this.startpos = startpos;
+            this.nelem = nelem;
+        }
+
+        @Override
+        public synchronized void run() {
+            // Println used to show the working ranges for debugging purposes
+            System.out.println("Working from position " + startpos + " to position " + (startpos + nelem - 1)); // NOPMD
+            for (int i = startpos; i < list.size() && i < startpos + nelem; i++) {
+                this.res += this.list.get(i);
+            }
+        }
+
+        /**
+         * Returns the result of summing up the integers within the list.
+         *
+         * @return the sum of every element in the array
+         */
+        public synchronized double getResult() {
+            return this.res;
+        }
+
+    }
+}
